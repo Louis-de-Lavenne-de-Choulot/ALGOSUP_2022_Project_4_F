09b78b1fa7360dda7bed7ec38d241f2ff6b8ef65
Polonit <71769482+Louis-de-Lavenne-de-Choulot@users.noreply.github.com>
2022-03-04 13:33:31 +0100

    Script animation

--- Project B3/Assets/Scripts/NPC.cs
+++ Project B3/Assets/Scripts/NPC.cs
@@ -9,17 +9,21 @@ public class NPC : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform[] goal;
-    public Transform agentPos;
     public UnityEngine.AI.NavMeshAgent agent;
+    Vector2 velocity = Vector2.zero;
+    Animator anim;
 
     void Start () {
-        agent.speed = 3;
-        agent.destination = goal[0].position;
+        anim = gameObject.GetComponent<Animator>();
+        // agent.speed = 3;
+        // agent.destination = goal[0].position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool shouldMove = velocity.magnitude > 0.5f && agent.remainingDistance > agent.radius;
+        anim.SetBool("IsMoving", shouldMove);
         Debug.DrawRay(agentPos.position, Vector3.forward, Color.red);
     }
 }
