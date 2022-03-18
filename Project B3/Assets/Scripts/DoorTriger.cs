389e7332a6e5b2263d63e4ff6fd3449e0b4e60e1
David Cuahonte Cuevas <david.cuahonte-cuevas@algosup.com>
2022-03-18 10:35:52 +0100

    Revert "Library done"
    This reverts commit ce1aa9bd08bc07a527b94c443a7b589ffd021fe0.

--- Project B3/Assets/Scripts/DoorTriger.cs
+++ Project B3/Assets/Scripts/DoorTriger.cs
@@ -8,29 +8,16 @@ public class DoorTriger : MonoBehaviour
 
     private float time = 0f;
 
-    [SerializeField]
-    private bool is_AI = true;
-
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other);
-        if (other.tag == "door" && is_AI)
-        {
-            Animator scanerR = other.transform.parent.parent.GetChild(1).gameObject.GetComponent(typeof(Animator)) as Animator;
-            Animator scanerL = other.transform.parent.parent.GetChild(2).gameObject.GetComponent(typeof(Animator)) as Animator;
-            Animator door = other.transform.parent.parent.gameObject.GetComponent(typeof(Animator)) as Animator;
 
-            scanerR.SetBool("isScan", true);
-            scanerL.SetBool("isScan", true);
-
-            door.SetBool("hasScanned", true);
-        }
     }
 
     private void Update()
     {
         time += Time.deltaTime;
     }
+
 
     private void OnTriggerStay(Collider other)
     {
