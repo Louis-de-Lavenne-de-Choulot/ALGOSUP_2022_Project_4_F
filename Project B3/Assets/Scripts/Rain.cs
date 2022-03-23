9b9a9a8a60d533c15be4a628197e7c47f3a9b208
Polonit <71769482+Louis-de-Lavenne-de-Choulot@users.noreply.github.com>
2022-03-23 12:00:36 +0100

    IA spawning

--- Project B3/Assets/Scripts/Rain.cs
+++ Project B3/Assets/Scripts/Rain.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 public class Rain : MonoBehaviour
 {
     public Transform[] englishRooms;
-    List<Transform> eR;
+    List<Transform> eR = new List<Transform>();
     public Transform[] languageLabs;
-    List<Transform> lL;
+    List<Transform> lL = new List<Transform>();
     public Transform softSkills;
-    List<Transform> sS;
+    List<Transform> sS = new List<Transform>();
     public Transform[] projectRooms;
-    Transform[] pR;
+    List<Transform> pR = new List<Transform>();
     public Transform[] Lunch;
-    List<Transform> l;
+    List<Transform> l = new List<Transform>();
     public Transform Auditorium;
-    List<Transform> a;
+    List<Transform> a = new List<Transform>();
     public Transform[] Toilettes;
-    List<Transform> t;
+    List<Transform> t = new List<Transform>();
     public GameObject[] toInit;
     Personae[] personaeNames;
     int[] personaeNumber;
@@ -69,23 +69,30 @@ public class Rain : MonoBehaviour
         sam = PlayerPrefs.GetInt("Sam", 0);
         basics = PlayerPrefs.GetInt("Basic", 0);
         personaeNumber = new int[10]{johnny, steph, alexandre, janka, nick, lindzy, denis, lana, sam, basics};
+        vv.GetComponent<UnityEngine.UI.Text>().text = "oui";
         foreach(Transform englishRoom in englishRooms){
             foreach(Transform findTrsfrm in englishRoom){
                 if (findTrsfrm.name == "Chair_Conference"){
+                    vv.GetComponent<UnityEngine.UI.Text>().text = "t1" ;
                     eR.Add(findTrsfrm);
+                    vv.GetComponent<UnityEngine.UI.Text>().text = "t2" ;
                 }
             }
         }
+        vv.GetComponent<UnityEngine.UI.Text>().text = "oui2" ;
         personaeNames = new Personae[9]{johnnyTT, stephTT, alexandreTT, jankaTT, nickTT, lindzyTT, denisTT, lanaTT, samTT};
+        Invoke("Invocation", 5f);
+    }
 
-        Invoke("Invocation", 5F);
+    void Update(){
     }
 
     private void Invocation(){
+        vv.GetComponent<UnityEngine.UI.Text>().text = "Pas de boucle";
         for (int persona = 0;  persona <  10; persona++){
             Personae p = personaeNames[persona];
             for (int i = 0; i < personaeNumber[persona]; i++){
-                GameObject obj = Instantiate(toInit[0], new Vector3(Random.Range(recept.position.x-12, recept.position.x+12), recept.position.y,Random.Range(recept.position.z-12, recept.position.z+12)), Quaternion.identity) as GameObject;
+                GameObject obj = Instantiate(toInit[persona], new Vector3(Random.Range(recept.position.x-12, recept.position.x+12), recept.position.y,Random.Range(recept.position.z-12, recept.position.z+12)), Quaternion.identity) as GameObject;
                 UnityEngine.AI.NavMeshAgent nobj = obj.GetComponent<UnityEngine.AI.NavMeshAgent>();
                 nobj.destination = eR[persona].position;
                 obj.transform.SetParent(gameObject.transform);
