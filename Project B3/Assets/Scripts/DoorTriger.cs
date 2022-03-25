96457e8f96dc4d0159e2ace80e0a1e3c62f260cb
maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
2022-03-25 15:16:05 +0100

    updated door scaner
    - made door always close if nobody is in the midle after 5s
    - made NPC door detection smaller
    - fixed some name in animator

--- Project B3/Assets/Scripts/DoorTriger.cs
+++ Project B3/Assets/Scripts/DoorTriger.cs
@@ -7,6 +7,7 @@ public class DoorTriger : MonoBehaviour
 {
 
     private float time = 0f;
+    private GameObject lastDoor = null;
 
     [SerializeField]
     private bool is_AI = true;
@@ -16,6 +17,8 @@ public class DoorTriger : MonoBehaviour
         Debug.Log(other);
         if (other.tag == "door" && is_AI)
         {
+            lastDoor = other.gameObject;
+
             Animator scanerR = other.transform.parent.parent.GetChild(1).gameObject.GetComponent(typeof(Animator)) as Animator;
             Animator scanerL = other.transform.parent.parent.GetChild(2).gameObject.GetComponent(typeof(Animator)) as Animator;
             Animator door = other.transform.parent.parent.gameObject.GetComponent(typeof(Animator)) as Animator;
@@ -24,31 +27,19 @@ public class DoorTriger : MonoBehaviour
             scanerL.SetBool("isScan", true);
 
             door.SetBool("hasScanned", true);
+            time = 0f;
         }
     }
 
     private void Update()
     {
         time += Time.deltaTime;
-    }
 
-    private void OnTriggerStay(Collider other)
-    {
-        if(other.tag == "door")
+        if (time > 5f && lastDoor != null)
         {
-            Animator door = other.transform.parent.parent.gameObject.GetComponent(typeof(Animator)) as Animator;
-            Debug.Log(door + " triger");
-            door.SetBool("doorOpen", true);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "door" && time > 5f)
-        {
-            Animator door = other.transform.parent.parent.gameObject.GetComponent(typeof(Animator)) as Animator;
-            Animator scanerR = other.transform.parent.parent.GetChild(1).gameObject.GetComponent(typeof(Animator)) as Animator;
-            Animator scanerL = other.transform.parent.parent.GetChild(2).gameObject.GetComponent(typeof(Animator)) as Animator;
+            Animator door = lastDoor.transform.parent.parent.gameObject.GetComponent(typeof(Animator)) as Animator;
+            Animator scanerR = lastDoor.transform.parent.parent.GetChild(1).gameObject.GetComponent(typeof(Animator)) as Animator;
+            Animator scanerL = lastDoor.transform.parent.parent.GetChild(2).gameObject.GetComponent(typeof(Animator)) as Animator;
 
             scanerR.SetBool("isScan", false);
             scanerR.SetBool("isScan fail", false);
@@ -66,6 +57,46 @@ public class DoorTriger : MonoBehaviour
             door.SetBool("doorOpen", false);
 
             time = 0f;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "door")
+        {
+            Animator door = other.transform.parent.parent.gameObject.GetComponent(typeof(Animator)) as Animator;
+            Debug.Log(door + " triger");
+            door.SetBool("doorOpen", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "door")
+        {
+            if(time > 5f)
+            {
+                Animator door = other.transform.parent.parent.gameObject.GetComponent(typeof(Animator)) as Animator;
+                Animator scanerR = other.transform.parent.parent.GetChild(1).gameObject.GetComponent(typeof(Animator)) as Animator;
+                Animator scanerL = other.transform.parent.parent.GetChild(2).gameObject.GetComponent(typeof(Animator)) as Animator;
+
+                scanerR.SetBool("isScan", false);
+                scanerR.SetBool("isScan fail", false);
+                scanerR.SetBool("reScan", false);
+                scanerR.SetInteger("count", 0);
+                scanerR.SetBool("isGreen", false);
+
+                scanerL.SetBool("isScan", false);
+                scanerL.SetBool("isScan fail", false);
+                scanerL.SetBool("reScan", false);
+                scanerL.SetInteger("count", 0);
+                scanerL.SetBool("isGreen", false);
+
+                door.SetBool("hasScanned", false);
+                door.SetBool("doorOpen", false);
+
+                time = 0f;
+            }
         }
     }
 }
