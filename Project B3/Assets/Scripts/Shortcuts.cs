66eeff81e53b012b5a9e7fafa61caacf2e4b67eb
MaximePagesAlgoSup <maxime.pages@algosup.com>
2022-03-30 14:57:45 +0200

    Shortcuts finally done

--- Project B3/Assets/Scripts/Shortcuts.cs
+++ Project B3/Assets/Scripts/Shortcuts.cs
@@ -3,56 +3,43 @@ using UnityEngine.SceneManagement;
 public class Shortcuts : MonoBehaviour
 {
     private float threshold = 3f;
-    private float xtime = 3f;
-    private bool lockx = false;
-    private float ytime = 3f;
-    private bool locky = false;
-    [SerializeField]
+    private float xtime;
+    private bool lockx;
+    private float ytime;
+    private bool locky;
     OVRManager manager;
     [SerializeField]
     Transform Spawn;
-    [SerializeField]
     Rigidbody body;
+
+    void Start()
+    {
+        manager = GameObject.Find("OVRCameraRig").GetComponent<OVRManager>();
+        body = GetComponent<Rigidbody>();
+        xtime = 3f;
+        ytime = 3f;
+        lockx = false;
+        locky = false;
+    }
 
     void Update()
     {
-        OVRInput.Update();
-        if(OVRInput.Get(OVRInput.Button.Three))
-        {
-            xtime -= Time.deltaTime;
-        }
-        else
-        {
-            if(xtime > 0f) Pause();
-            xtime = threshold;
-            lockx = false;
-        }
-        if(OVRInput.Get(OVRInput.Button.Four))
-        {
-            ytime -= Time.deltaTime;
-        }
-        else
-        {
-            ytime = threshold;
-            locky = false;
-        }
-        if(xtime < 0.2f && ytime < 0.2f)
+        if (OVRInput.GetDown(OVRInput.Button.Three)) xtime = threshold;
+        if (OVRInput.GetDown(OVRInput.Button.Four)) ytime = threshold;
+        if (OVRInput.GetUp(OVRInput.Button.Three) && xtime > 0f) Pause();
+        if (OVRInput.Get(OVRInput.Button.Three)) xtime -= Time.deltaTime;
+        if (OVRInput.Get(OVRInput.Button.Four)) ytime -= Time.deltaTime;
+        if (!lockx && xtime < 0f) Respawn();
+        if (!locky && ytime < 0f) ExitScenario();
+        if (OVRInput.Get(OVRInput.Button.Three) &&
+            OVRInput.Get(OVRInput.Button.Four) &&
+            xtime < 0.2f && ytime < 0.2f)
         {
             xtime = threshold;
             ytime = threshold;
             lockx = true;
             locky = true;
             MainMenu();
-        }
-        if(!lockx && xtime < 0f)
-        {
-            lockx = true;
-            Respawn();
-        }
-        if(!locky && ytime < 0f)
-        {
-            locky = true;
-            ExitScenario();
         }
     }
     void MainMenu()
