b30060f71d8e676671a0e53f5dbbfaa5cc46869c
Polonit <71769482+Louis-de-Lavenne-de-Choulot@users.noreply.github.com>
2022-03-30 10:05:27 +0200

    script Vector3 to Transform
    adaptation for the animations

--- Project B3/Assets/Scripts/NPC.cs
+++ Project B3/Assets/Scripts/NPC.cs
@@ -7,7 +7,7 @@ public class NPC : MonoBehaviour
     // Start is called before the first frame update
     float cooldown = 60F;
     float period;
-    public Vector3[] goal = new Vector3[11];
+    public Transform[] goal = new Transform[11];
     Transform agentPos;
     Vector3 agentLastPos;
     UnityEngine.AI.NavMeshAgent agent;
@@ -26,7 +26,7 @@ public class NPC : MonoBehaviour
         anim = gameObject.GetComponent<Animator>();
         agentPos = gameObject.GetComponent<Transform>();
         agent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
-        agent.destination = goal[PlayerPrefs.GetInt("day", 0)*2-1];
+        agent.destination = goal[PlayerPrefs.GetInt("day", 0)*2-1].position;
         InvokeRepeating("TimeCheck", 20f, 5f);
     }
 
@@ -131,7 +131,7 @@ public class NPC : MonoBehaviour
 
     void TimeCheck(){
         if (GameTime.intTimer > times[timeNumber]){
-            agent.destination = goal[timeNumber];
+            agent.destination = goal[timeNumber].position;
             timeNumber++;
         }
     }
