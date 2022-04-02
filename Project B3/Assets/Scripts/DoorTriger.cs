b3eea60993f7439bd3cb2f99c67346147ff208ac
maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
2022-04-02 18:38:05 +0200

    fixed eror spam
    - removed a few too many layer and tag assosiation
    - renamed entrance Frame in Glass Frame

--- Project B3/Assets/Scripts/DoorTriger.cs
+++ Project B3/Assets/Scripts/DoorTriger.cs
@@ -56,7 +56,7 @@ public class DoorTriger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "door" && is_AI)
+        if (other.gameObject.layer == LayerMask.NameToLayer("Door") && is_AI)
         {
             Animator scanerR = other.transform.parent.parent.GetChild(1).gameObject.GetComponent(typeof(Animator)) as Animator;
             Animator scanerL = other.transform.parent.parent.GetChild(0).gameObject.GetComponent(typeof(Animator)) as Animator;
@@ -192,7 +192,7 @@ public class DoorTriger : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if(other.tag == "door")
+        if(other.gameObject.layer == LayerMask.NameToLayer("Door"))
         {
             Animator door = other.transform.parent.parent.gameObject.GetComponent(typeof(Animator)) as Animator;
             door.SetBool("doorOpen", true);
@@ -206,7 +206,7 @@ public class DoorTriger : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "door")
+        if (other.gameObject.layer == LayerMask.NameToLayer("Door"))
         {
             Animator door = other.transform.parent.parent.gameObject.GetComponent(typeof(Animator)) as Animator;
             Animator scanerR = other.transform.parent.parent.GetChild(1).gameObject.GetComponent(typeof(Animator)) as Animator;
