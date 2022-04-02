aa2332b4b91449da8e095e33327d1c1d1b6d8417
Polonit <71769482+Louis-de-Lavenne-de-Choulot@users.noreply.github.com>
2022-04-02 03:53:39 +0200

    Rain.Cs fixed

--- Project B3/Assets/Scripts/Rain.cs
+++ Project B3/Assets/Scripts/Rain.cs
@@ -1,27 +1,26 @@
 using System.Collections;
-using System.Reflection;
 using System.Collections.Generic;
 using UnityEngine;
 public class Rain : MonoBehaviour
 {
     public Transform trash;
+    public Transform[] englishRooms;
+    List<Transform> eR = new List<Transform>();
+    int[] eR2 = new int[5]{0,0,0,0,0};
+    public Transform[] languageLabs;
+    List<Transform> lL = new List<Transform>();
+    int[] lL2 = new int[5]{0,0,0,0,0};
     public Transform softSkills;
     List<Transform> sS = new List<Transform>();
     int[] sS2 = new int[5]{0,0,0,0,0};
-    public Transform languageLabs;
-    List<Transform> lL = new List<Transform>();
-    int[] lL2 = new int[5]{0,0,0,0,0};
-    public Transform Lunch;
+    public Transform[] projectRooms;
+    List<Transform> pR = new List<Transform>();
+    int[] pR2 = new int[5]{0,0,0,0,0};
+    public Transform[] Lunch;
     List<Transform> l = new List<Transform>();
     public Transform Auditorium;
     List<Transform> a = new List<Transform>();
     int[] a2 = new int[5]{0,0,0,0,0};
-    public Transform[] englishRooms;
-    List<Transform> eR = new List<Transform>();
-    int[] eR2 = new int[5]{0,0,0,0,0};
-    public Transform[] projectRooms;
-    List<Transform> pR = new List<Transform>();
-    int[] pR2 = new int[5]{0,0,0,0,0};
     public Transform[] Toilettes;
     List<Transform> t = new List<Transform>();
     public GameObject[] toInit;
@@ -38,12 +37,10 @@ public class Rain : MonoBehaviour
     int sam;
     int basics;
     int maxNumber;
-    Color[] Skin = new Color[4]{new Color(0.254717f, 0.1748036f, 0.08290314f), new Color(0.727f, 0.6063917f, 0.4154285f), new Color(0.1792453f, 0.07965653f, 0.03297437f), new Color(0.8235294f, 0.6969679f, 0.5583529f)};
     public Transform recept;
+    Color[] Skin = new Color[4]{new Color(0.254717f, 0.1748036f, 0.08290314f), new Color(0.727f, 0.6063917f, 0.4154285f), new Color(0.1792453f, 0.07965653f, 0.03297437f), new Color(0.8235294f, 0.6969679f, 0.5583529f)};
     int timeNumber = 0;
     int[] times = new int[8]{85, 100, 160, 280, 340, 355, 415, 440};
-
-    private Personae basicsTT = new Personae('I', 'P', 'P', 'P', 'P', 'S', 'S', 'C', 'C', 'P', 'E');
     // G = get/order food, O = outside, I = inside, B = bring his meal
     private Personae johnnyTT = new Personae('G', 'E', 'P', 'S', 'S', 'P', 'P', 'C', 'C', 'P', 'P');
 
@@ -66,9 +63,9 @@ public class Rain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        maxNumber = PlayerPrefs.GetInt("MaxAi", 0);
-        johnny = PlayerPrefs.GetInt("Johnny", 0);
-        steph = PlayerPrefs.GetInt("Steph", 0);
+        maxNumber = 300;//PlayerPrefs.GetInt("MaxAi", 0);
+        johnny = 60;//PlayerPrefs.GetInt("Johnny", 0);
+        steph = 20;//PlayerPrefs.GetInt("Steph", 0);
         alexandre = PlayerPrefs.GetInt("Alexandre", 0);
         janka = PlayerPrefs.GetInt("Janka", 0);
         nick = PlayerPrefs.GetInt("Nick", 0);
@@ -77,26 +74,25 @@ public class Rain : MonoBehaviour
         lana = PlayerPrefs.GetInt("Lana", 0);
         sam = PlayerPrefs.GetInt("Sam", 0);
         basics = PlayerPrefs.GetInt("Basic", 0);
-        personaeNumber = new int[10]{basics, johnny, alexandre, sam, nick, denis, lana, steph, janka, lindzy};
-        foreach(Transform findTrsfrm in languageLabs){
-            if (findTrsfrm.name == "Chair"){
-                lL.Add(findTrsfrm);
-            }
-        }
-        foreach(Transform findTrsfrm in Auditorium){
-                if (findTrsfrm.name == "Chair"){
-                    a.Add(findTrsfrm);
-                }
-        }
-        foreach(Transform findTrsfrm in softSkills){
-            if (findTrsfrm.name == "Chair"){
-                sS.Add(findTrsfrm);
-            }
-        }
+        personaeNumber = new int[10]{johnny, steph, alexandre, janka, nick, lindzy, denis, lana, sam, basics};
         foreach(Transform englishRoom in englishRooms){
             foreach(Transform findTrsfrm in englishRoom){
                 if (findTrsfrm.name == "Chair"){
                     eR.Add(findTrsfrm);
+                }
+            }
+        }
+        foreach(Transform languageLab in languageLabs){
+            foreach(Transform findTrsfrm in languageLab){
+                if (findTrsfrm.name == "Chair"){
+                    lL.Add(findTrsfrm);
+                }
+            }
+        }
+        foreach(Transform softSkill in softSkills){
+            foreach(Transform findTrsfrm in softSkill){
+                if (findTrsfrm.name == "Chair"){
+                    sS.Add(findTrsfrm);
                 }
             }
         }
@@ -107,7 +103,12 @@ public class Rain : MonoBehaviour
                 }
             }
         }
-        personaeNames = new Personae[10]{basicsTT, johnnyTT, alexandreTT, samTT, nickTT, denisTT, lanaTT, stephTT, jankaTT, lindzyTT};
+        foreach(Transform findTrsfrm in Auditorium){
+                if (findTrsfrm.name == "Chair"){
+                    a.Add(findTrsfrm);
+                }
+        }
+        personaeNames = new Personae[9]{johnnyTT, stephTT, alexandreTT, jankaTT, nickTT, lindzyTT, denisTT, lanaTT, samTT};
         Invocation();
         InvokeRepeating("TimeCheck", 20f, 5f);
     }
@@ -116,7 +117,7 @@ public class Rain : MonoBehaviour
     }
 
     private void Invocation(){
-        for (int persona = 0;  persona < 10; persona++){
+        for (int persona = 0;  persona < 9; persona++){
             Personae p = personaeNames[persona];
             for (int i = 0; i < personaeNumber[persona]; i++){
                 GameObject obj = Instantiate(toInit[persona], new Vector3(Random.Range(recept.position.x-12, recept.position.x+12), recept.position.y,Random.Range(recept.position.z-12, recept.position.z+12)), Quaternion.identity) as GameObject;
@@ -129,7 +130,7 @@ public class Rain : MonoBehaviour
                         obj.transform.GetChild(child).gameObject.SetActive(true);
                 }
                 obj.transform.GetChild(act[0]).GetComponent<SkinnedMeshRenderer>().material.color =  Sk;
-                if(persona < 5){
+                if((persona+1)%2 != 0){
                     obj.transform.GetChild(act[2]).GetComponent<SkinnedMeshRenderer>().material.color =  Sk;
                 }else{
                     obj.transform.GetChild(act[2]).GetComponent<SkinnedMeshRenderer>().materials[1].color =  Sk;
@@ -176,7 +177,7 @@ public class Rain : MonoBehaviour
         else if(moment == 'C' && a.Count > a2[numb]){
             Transform temp = a[a2[numb]];
             a2[numb]++;
-            return temp;
+            return temp;                         
         }
         Debug.Log("trash");
         return trash;
